d2ca06aa80f87dc9b5b2f72dfa0e30763fffa5ee
Kelvin Uzo <b00141682@mytudublin.ie>
2024-10-02 12:10:13 +0100

    first commit

---
+++ nestedForLoop.php
@@ -0,0 +1,14 @@
+<?php
+echo "<TABLE border='1'>";
+for ($i=1; $i<=20;$i++){
+    echo "<TR>";
+    
+    for ($j=1;$j<=10;$j++){
+        echo "<TD>Row $i, Col $j</TD>";
+        
+    }
+    echo "</TR>";
+}
+echo "</TABLE>";
+
+?>
